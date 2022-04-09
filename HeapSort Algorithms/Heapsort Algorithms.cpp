d42db91f8c9b3c72e11e612a061dea22e4874e00
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-04-09 23:50:48 +0530

    Update Heapsort Algorithms.cpp

--- HeapSort Algorithms/Heapsort Algorithms.cpp
+++ HeapSort Algorithms/Heapsort Algorithms.cpp
@@ -28,7 +28,7 @@ void Heapify(vector<int>& heap,int curr,int size)
     }
 }
 
-void Heapsort(vector<int>& heap)
+void Heapsort(vector<int>& heap,int comparision=0)
 {
     //Build heap (rearrange array)
     for(int i=heap.size()/2-1;i >= 0;--i)
@@ -41,12 +41,14 @@ void Heapsort(vector<int>& heap)
         heap[i] = temp;
 
         Heapify(heap,0,i); //Heapify root with heapsize = i
+        comparision++;
     }
+    cout<<"Number of comparisions are :"<<comparision<<endl;
 }
 
 int main()
 {
-    vector<int> heap{9,6,8,2,1,4,3}; //Max heap
+    vector<int> heap{9,6,8,2,1,4,3,7}; //Max heap
     Heapsort(heap);
 
     cout<<"heap is ASC"<<endl;
@@ -56,3 +58,36 @@ int main()
     }
     return 0;
 }
+
+
+
+// its parent floor( (x - 1) / 2 )
+// its left child 2x + 1
+// its right child 2x + 2
+// The index of the last non-leaf node of a n-sized heap is floor( n / 2 ) - 1.
+
+	// void siftDown(vector<int>& nums, int n, int i){
+    //     int biggest = i;
+    //     int l = 2 * i + 1;
+    //     int r = 2 * i + 2;
+    //     if(l < n && nums[biggest] < nums[l])
+    //         biggest = l;
+    //     if(r < n && nums[biggest] < nums[r])
+    //         biggest = r;
+    //     if(biggest != i){
+    //         swap(nums[i], nums[biggest]);
+    //         siftDown(nums, n, biggest);
+    //     }
+    // }
+    
+	// // heapSort(nums);
+    // void heapSort(vector<int>& nums){
+    //     // heapify stage (bottom up approach)
+    //     for(int i = nums.size() / 2 - 1; i >= 0; i--)
+    //         siftDown(nums, nums.size(), i);
+    //     // sorting stage
+    //     for(int i = nums.size() - 1; i > 0; i--){
+    //         swap(nums[0], nums[i]);
+    //         siftDown(nums, i, 0);
+    //     }
+    // }
